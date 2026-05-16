5ec2b1f157bd8e77b05edb7bc209f5ed938d2ab9
Shartrooper <michinaux.marko@gmail.com>
2026-05-15 20:32:03 -0400

    config: implement rolldown vite bundling

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
